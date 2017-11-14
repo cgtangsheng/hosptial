3fabec4cdde856bacb63b2c302a7c5722cd4a90c
tomstang <tomstang@tencent.com>
2017-11-14 09:06:17 +0800

    report

--- controllers/DmController.php
+++ controllers/DmController.php
@@ -36,7 +36,8 @@ class DmController extends Controller
         //评估饮食状况
         $data = $this->checkDietInfo($param);
         $result = array_merge($result,$data);
-
+//var_dump(json_encode($result));
+//exit;
         return $this->render('check',["data"=>$result]);
     }
 
@@ -46,75 +47,171 @@ class DmController extends Controller
         switch ($param["work_density"]){
             case 0:
                 if($param["diet"]>5){
-                    $diet_desc[]="主食太多";
+                    $diet_desc[]=array(
+                        "label"=>"主食",
+                        "desc"=>"主食太多",
+                        "standard"=>"轻体力劳动每日摄入标准4~5两",
+                        "value"=>$param["diet"],
+                    );
                 }
                 break;
             case 1:
                 if($param["diet"]>6){
-                    $diet_desc[]="主食太多";
+                    $diet_desc[]=array(
+                        "label"=>"主食",
+                        "desc"=>"主食太多",
+                        "standard"=>"轻体力劳动每日摄入标准5~6两",
+                        "value"=>$param["diet"],
+                    );
                 }
                 break;
             case 2:
                 if($param["diet"]>8){
-                    $diet_desc[]="主食太多";
+                    $diet_desc[]=array(
+                        "label"=>"主食",
+                        "desc"=>"主食太多",
+                        "standard"=>"中体力劳动每日摄入标准6~8两",
+                        "value"=>$param["diet"],
+
+                    );
                 }
                 break;
         }
         if($param["vegetables"]<10){
-            $diet_desc[]="蔬菜摄入太少";
+            $diet_desc[]=array(
+                "label"=>"蔬菜",
+                "desc"=>"蔬菜摄入太少",
+                "standard"=>"一市斤以上",
+                "value"=>$param["vegetables"],
+            );
         }
         if($param["milk"]<250){
-            $diet_desc[]="牛奶摄入太少";
+            $diet_desc[]=array(
+                "label"=>"牛奶",
+                "desc"=>"牛奶摄入太少",
+                "standard"=>"250ml",
+                "value"=>$param["milk"],
+            );
         }
         if($param["egg"]<1){
-            $diet_desc[]="鸡蛋摄入太少";
+            $diet_desc[]=array(
+                "label"=>"蛋类",
+                "desc"=>"蛋类摄入太少",
+                "standard"=>"1个",
+                "value"=>$param["egg"],
+            );
         }
-        if($param["meet"]<2){
-            $diet_desc[]="瘦肉摄入太少";
+        if($param["meet"]!=false&&$param["meet"]<2){
+            $diet_desc[]=array(
+                "label"=>"瘦肉",
+                "desc"=>"瘦肉摄入太少",
+                "standard"=>"2两",
+                "value"=>$param["meet"],
+            );
         }
-        if($param["bean"]<1){
-            $diet_desc[]="豆制品摄入太少";
+        if($param["bean"]!=false&&$param["bean"]<1){
+            $diet_desc[]=array(
+                "label"=>"豆制品",
+                "desc"=>"豆制品摄入太少",
+                "standard"=>"1~2两",
+                "value"=>$param["bean"],
+            );
         }elseif($param["bean"]>2){
-            $diet_desc[]="豆制品摄入太多";
+            $diet_desc[]=array(
+                "label"=>"豆制品",
+                "desc"=>"豆制品摄入太多",
+                "standard"=>"2~3汤匙",
+                "value"=>$param["bean"],
+            );
         }
         if($param["oil"]>3){
-            $diet_desc[]="食用油摄入太多";
+            $diet_desc[]=array(
+                "label"=>"食用油",
+                "desc"=>"食用油摄入太多",
+                "standard"=>"2~3汤匙",
+                "value"=>$param["oil"],
+            );
         }
         if($param["salt"]>6){
-            $diet_desc[]="盐摄入太多";
+            $diet_desc[]=array(
+                "label"=>"食盐",
+                "desc"=>"盐摄入太多",
+                "standard"=>"每日摄入标准不超过g",
+                "value"=>$param["salt"],
+            );
         }
-        $data["diet_info"]=$diet_desc;
-        return $data;
+        return $diet_desc;
     }
 
     public function checkBmiInfo($param){
         $data = array();
+        $bmi=0;
         if($param["height"]!=false && $param["weight"]!=false){
-            $data["bmi"]=sprintf("%0.2f",floatval($param["weight"])/floatval($param["height"])*floatval($param["height"]));
+            $bmi=sprintf("%0.2f",floatval($param["weight"])/floatval($param["height"])*floatval($param["height"]));
         }
-        if($data["bmi"]<18){
-            $data["bmi_desc"]="体重不足";
-        }else if($data["bmi"]>=18 && $data["bmi"]<=22.9){
-            $data["bmi_desc"]="体重正常";
-        }else if($data["bmi"]>22.9 && $data["bmi"]<25){
-            $data["bmi_desc"]="一般超重";
-        }else if($data["bmi"]>=25 && $data["bmi"]<=29.9){
-            $data["bmi_desc"]="肥胖前期";
+        if($bmi<18){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"体重不足",
+                "value"=>$bmi,
+                "standard"=>"18~22.9"
+            );
+        }else if($bmi>=18 && $bmi<=22.9){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"体重正常",
+                "value"=>$bmi,
+                "standard"=>"18~22.9"
+            );
+        }else if($bmi>22.9 && $bmi<25){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"一般超重",
+                "standard"=>"22.9~25",
+                "value"=>$bmi,
+            );
+        }else if($bmi>=25 && $bmi<=29.9){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"肥胖前期",
+                "standard"=>"25~29.9",
+                "value"=>$bmi,
+            );
         }
-        else if($data["bmi"]>29.9 && $data["bmi"]<=34.9){
-            $data["bmi_desc"]="肥胖I期";
+        else if($bmi>29.9 && $bmi<=34.9){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"肥胖I期",
+                "standard"=>"29.9~34.9",
+                "value"=>$bmi,
+            );
         }
-        else if($data["bmi"]>34.9 && $data["bmi"]<39.9){
-            $data["bmi_desc"]="肥胖II期";
+        else if($bmi>34.9 && $bmi<39.9){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"肥胖II期",
+                "standard"=>"34.9~39.9",
+                "value"=>$bmi,
+            );
         }
-        else if($data["bmi"]>39.9){
-            $data["bmi_desc"]="肥胖III期";
+        else if($bmi>39.9){
+            $data[]=array(
+                "label"=>"BMI",
+                "desc"=>"肥胖II期",
+                "standard"=>"大于39.9",
+                "value"=>$bmi,
+            );
         }
         if($param["hip"]!=false && $param["waistline"]!=false){
-            $data["hip_waistline"]=sprintf("%0.2f",floatval($param["waistline"])/floatval($param["hip"]));
+            //$data["hip_waistline"]=sprintf("%0.2f",floatval($param["waistline"])/floatval($param["hip"]));
         }
         if($param["sex"] == 0 && $param["waistline"]>85 || $param["sex"] == 1 && $param["waistline"]>80){
-            $data["waistline_desc"]="潜在向心型肥胖";
+            $data[]=array(
+                "label"=>"腰围",
+                "desc"=>"潜在向心型肥胖",
+                "standard"=>"男>85cm,女>80cm",
+                "value"=>$param["waistline"],
+            );
         }
         return $data;
     }
